5cf2274eb0fe75df958926d7e83c488901265929
Xenia <lommy101@gmail.com>
2020-04-09 15:02:51 +0900

    add signup page & main

--- Web/sample.py
+++ Web/sample.py
@@ -1,1 +1,38 @@
-# Flask based web Server
+from flask import Flask, render_template, session, redirect, url_for, request
+from markupsafe import escape
+
+app = Flask(__name__)
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+@app.route('/')
+def main():
+    #if 'username' in session:
+        #return 'Logged in as %s' % escape(session['username'])
+    return render_template('main.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        print(request.form['username'])
+        print(request.form['userpassword'])
+        #session['username'] = request.form['username']
+        return redirect(url_for('main'))
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        print(request.form['username'])
+        print(request.form['userpassword'])
+        print(request.form['re_userpassword'])
+        #session['username'] = request.form['username']
+        return redirect(url_for('main'))
+    return render_template('signup.html')
+
+@app.route('/logout')
+def logout():
+    #session.pop('username', None)
+    return redirect(url_for('main'))
+
+if __name__ == '__main__':
+    app.run()
