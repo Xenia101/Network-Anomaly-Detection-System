8d3a02c7cb83bec782c5977159ad347edea172f2
Xenia <lommy101@gmail.com>
2020-04-09 15:22:54 +0900

    session login

--- Web/sample.py
+++ Web/sample.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, session, redirect, url_for, request
+from flask import Flask, render_template, session, redirect, url_for, request, flash
 from markupsafe import escape
 
 app = Flask(__name__)
@@ -6,16 +6,16 @@ app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
 @app.route('/')
 def main():
-    #if 'username' in session:
-        #return 'Logged in as %s' % escape(session['username'])
-    return render_template('main.html')
+    if 'username' in session:
+        return render_template('main.html', is_anonymous=0)
+    return render_template('main.html', is_anonymous=1)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         print(request.form['username'])
         print(request.form['userpassword'])
-        #session['username'] = request.form['username']
+        session['username'] = request.form['username']
         return redirect(url_for('main'))
     return render_template('login.html')
 
@@ -25,13 +25,13 @@ def signup():
         print(request.form['username'])
         print(request.form['userpassword'])
         print(request.form['re_userpassword'])
-        #session['username'] = request.form['username']
+        session['username'] = request.form['username']
         return redirect(url_for('main'))
     return render_template('signup.html')
 
 @app.route('/logout')
 def logout():
-    #session.pop('username', None)
+    session.pop('username', None)
     return redirect(url_for('main'))
 
 if __name__ == '__main__':
