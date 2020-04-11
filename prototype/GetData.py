893b7617590472d01ae890ced69b123ce77aeb61
Xenia <lommy101@gmail.com>
2020-04-12 01:56:35 +0900

    add somethings..

--- prototype/GetData.py
+++ prototype/GetData.py
@@ -1,4 +1,26 @@
 import pyshark
-pcap_reader = pyshark.LiveCapture('이더넷')
-for packet in pcap_reader.sniff_continuously():
-    print(packet)
+cap = pyshark.LiveCapture('이더넷')
+
+def network_conversation(packet):
+  try:
+    protocol = packet.transport_layer
+    source_address = packet.ip.src
+    source_port = packet[packet.transport_layer].srcport
+    destination_address = packet.ip.dst
+    destination_port = packet[packet.transport_layer].dstport
+    return (f'{protocol} {source_address}:{source_port} --> {destination_address}:{destination_port}')
+  except AttributeError as e:
+    pass
+
+for packet in cap.sniff_continuously():
+  #print(packet)
+  #break
+  #try:
+   #if packet.transport_layer is not None:
+      #print(packet.ip)
+  #except AttributeError as e:
+  #  pass
+  results = network_conversation(packet)
+  if results != None:
+      print(results)
+
