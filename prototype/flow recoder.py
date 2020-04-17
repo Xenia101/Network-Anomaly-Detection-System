32a4dac6b57eae33b74cb6ccb949aeca8d5040c1
Xenia <lommy101@gmail.com>
2020-04-17 18:38:20 +0900

    return to type of json

--- prototype/flow recoder.py
+++ prototype/flow recoder.py
@@ -6,7 +6,7 @@ from scapy.all import *
 from tcp_stream import TCPStream
 import argparse
 
-attrs = ['src','sport','dst','dport','proto','push_flag_ratio','average_len','average_payload_len','pkt_count','flow_average_inter_arrival_time','kolmogorov','shannon']
+attrs = ['src','sport','dst','dport','proto','push_flag_ratio','average_len','average_payload_len','pkt_count','flow_average_inter_arrival_time']
 
 def proto_name(sport,dport,use_dpi=False,payload=None):
     if dport == 80 or sport == 80:
@@ -26,15 +26,30 @@ def create_reverse_flow_key(pkt):
 def create_flow_keys(pkt):
     return create_forward_flow_key(pkt),create_reverse_flow_key(pkt)
 
-def gen_json(flow):
-    print(flow)
-    #return json
-
+def gen_json(flows):
+    result = dict()
+    index = 1
+    for flow in flows.values():
+        data = dict()
+        data['src']                     = flow.src
+        data['sport']                   = flow.sport
+        data['dst']                     = flow.dst
+        data['dport']                   = flow.dport
+        data['proto']                   = flow.proto
+        data['proto_name']              = proto_name(flow.sport,flow.dport)
+        data['push_flag_ratio']         = round(flow.push_flag_ratio(),2)
+        data['avrg_len']                = round(flow.avrg_len(),2)
+        data['avrg_payload_len']        = round(flow.avrg_payload_len(),2)
+        data['pkt_count']               = flow.pkt_count
+        data['avrg_inter_arrival_time'] = flow.avrg_inter_arrival_time()
+        result[index] = data
+        index += 1
+    return json.dumps(result, indent=4, sort_keys=False)
+    
 def get_data(path):
     packets = rdpcap(path)
     flows = dict()
     packets = [ pkt for pkt in packets if IP in pkt for p in pkt if TCP in p ]
-
     for pkt in packets:
         flow_tuple = reverse_flow_tuple = key_to_search = None
         flow_tuple,reverse_flow_tuple = create_flow_keys(pkt[IP])
@@ -51,23 +66,8 @@ def get_data(path):
         else:
             tcp_stream.add(pkt[IP])
         flows[flow_key] = tcp_stream
-
     return flows
-    
-    #for flow in flows.values():
-    #    print(proto_name(flow.sport,flow.dport))
-    #    print(flow.src)
-    #    print(flow.sport)
-    #    print(flow.dst)
-    #    print(flow.dport)
-    #    print(flow.proto)
-    #    print(flow.push_flag_ratio())
-    #    print(flow.avrg_len())
-    #    print(flow.avrg_payload_len())
-    #    print(flow.pkt_count)
-    #    print(flow.avrg_inter_arrival_time())
-    #    break
     
 if __name__ == "__main__":
     data = get_data("pcap_files/example.pcap")
-    gen_json(data)
+    print(gen_json(data))
