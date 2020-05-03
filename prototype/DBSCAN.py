4815320373b926b424791f52b412425d7ec9dadc
Xenia <lommy101@gmail.com>
2020-05-04 00:21:51 +0900

    DBSCAN

--- prototype/DBSCAN.py
+++ prototype/DBSCAN.py
@@ -12,3 +12,19 @@ measurements = [x for x in data.values()]
 vec = DictVectorizer()
 X = vec.fit_transform(measurements).toarray()
 
+# DBSCAN
+from sklearn.cluster import DBSCAN
+eps = 4
+min_samples = 2
+db=DBSCAN(eps=eps, min_samples=min_samples, metric='euclidean')
+y_db = db.fit_predict(X)
+print("Number of Data : {0}\n".format(len(db.labels_)))
+print("params : eps = {0}, min_samples = {1}\n". format(eps,min_samples))
+print('※ cluster labels : -1 is noise point\n')
+print('> OUTPUT')
+print(db.labels_)
+
+print('\nnumber of clusters : ', end='')
+labels = db.labels_
+n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+print(n_clusters_)
