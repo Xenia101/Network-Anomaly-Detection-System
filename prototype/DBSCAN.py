a22616fe4aa9acd9732fabc19b391df414d88c80
Xenia <lommy101@gmail.com>
2020-05-04 00:17:40 +0900

    Deleted getdata.py

---
+++ prototype/DBSCAN.py
@@ -0,0 +1,14 @@
+from FlowRecoder import get_data, gen_json
+import json
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction import DictVectorizer
+import matplotlib.pyplot as plt
+
+data = get_data("pcap_files/example.pcap")
+data = json.loads(gen_json(data))
+
+measurements = [x for x in data.values()]
+vec = DictVectorizer()
+X = vec.fit_transform(measurements).toarray()
+
