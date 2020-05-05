f53394e14ddb3d8d56da4b6ed392835c0756ba28
Xenia <lommy101@gmail.com>
2020-05-05 22:46:27 +0900

    Add K-means.py

--- prototype/K-means.py
+++ prototype/K-means.py
@@ -11,3 +11,11 @@ def get_X(path):
 normal = get_X("./CIC-output/smallFlows.pcap_Flow.csv")
 anomaly = get_X("./CIC-output/http-flood.pcap_Flow.csv")
 X = np.r_['0',normal,anomaly]
+
+kmeans = KMeans(n_clusters=2).fit(X)
+y = kmeans.labels_
+# X.shape (1327, 78)
+# y.shape (1327, )
+
+plt.scatter(X[:,3], y, c=kmeans.predict(X), s=50, cmap='viridis');
+plt.show()
