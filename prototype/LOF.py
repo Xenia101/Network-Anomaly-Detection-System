834668e8acf37747f538076c803dcfc2b6d3f86f
XENIA <lommy101@gmail.com>
2020-05-08 01:12:07 +0900

    Update LOF.py

--- prototype/LOF.py
+++ prototype/LOF.py
@@ -7,21 +7,20 @@ from sklearn.neighbors import LocalOutlierFactor
 def percentage(p, w):
     return int(p*w/100)
 
-path = "./CIC-output/packet-new.pcap_Flow.csv"
+path = "./CIC-output/packet-0507.pcap_Flow.csv"
 
-X = Preprocessing.load_data(path)
+X = Preprocessing.load_df(path)
 
 clf = LocalOutlierFactor(n_neighbors=3, contamination=0.1)
-y_pred = clf.fit_predict(X)
+y_pred = clf.fit_predict(X.values)
 X_scores = clf.negative_outlier_factor_
 X_scores = np.array(X_scores, dtype=np.float64)
 
-df = Preprocessing.load_df(path)
-df['scores'] = -X_scores
+X['scores'] = -X_scores
 
 ## to CSV
-#df.to_csv('output.csv', index=False)
+#X.to_csv('output.csv', index=False)
 
 number1_percent = percentage(1, len(X_scores))
-print(df.sort_values(by='scores', ascending=False).head(number1_percent)['scores'])
+print(X.sort_values(by='scores', ascending=False).head(number1_percent)['scores'])
 
