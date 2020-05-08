f1d0dd31b3ad582a34f2ee0f6642b8ec2e1bd989
XENIA <lommy101@gmail.com>
2020-05-09 01:06:32 +0900

    Update LOF.py

--- prototype/LOF.py
+++ prototype/LOF.py
@@ -14,47 +14,58 @@ def train():
     path = "./CIC-output/packet-0507.pcap_Flow.csv"
     X = Preprocessing.load_df(path)
 
-    clf = LocalOutlierFactor(n_neighbors=3, contamination=0.1)
+    #LOF
+    clf = LocalOutlierFactor(n_neighbors=2, contamination=0.1)
     y_pred = clf.fit_predict(X.values)
     X_scores = clf.negative_outlier_factor_
     X_scores = np.array(X_scores, dtype=np.float64)
 
     X['scores'] = -X_scores
-    
+
+    columns = list(X.columns)
     number1_percent = percentage(10, len(X_scores))
-    return X, X.sort_values(by='scores', ascending=False).head(number1_percent)['scores']
+    return X, X.sort_values(by='scores', ascending=False).head(number1_percent)['scores'], columns
 
-def test(X, cutline):
+def test(X, cutline, columns):
     X.drop("scores", axis='columns', inplace=True)
-    anomaly_path = "./CIC-output/http-flood.pcap_Flow.csv"
+    anomaly_path = "./CIC-output/normal-1.pcap_Flow.csv"
     anomaly_data = Preprocessing.load_df(anomaly_path)
-
+    
+    result = []
+    c = 1
     for xi in range(len(anomaly_data)):
         
         nth_data = list()
         for x in list(anomaly_data.columns):
             nth_data.append(anomaly_data.loc[xi,x])
-        X.loc[len(X)] = nth_data 
+        X.loc[len(X)] = nth_data
 
-        clf = LocalOutlierFactor(n_neighbors=3, contamination=0.1)
+        #LOF
+        clf = LocalOutlierFactor(n_neighbors=2, contamination=0.1)
         y_pred = clf.fit_predict(X.values)
         X_scores = clf.negative_outlier_factor_
         X_scores = np.array(X_scores, dtype=np.float64)
 
-        print(-X_scores[-1])
+        print("[{}] - {}".format(c, -X_scores[-1]))
         if -X_scores[-1] >= cutline:
-            print('OUTtlier')
+            result.append(-1) # out
+            c+=1
         else:
-            print('INlier')
+            result.append(1) # in
+            c+=1
         print('=====')
         X = X.drop(X.index[len(X)-1])
-        
+    print("\n\n")
+    print("FileName : {}".format(anomaly_path))
+    print(result)
+    print("-1 : {}".format(result.count(-1)))
+    print("1 : {}".format(result.count(1)))
     return ''
 
 def cutline_score(scores):
     return scores.min(axis=0)
 
-X, train_score_1per = train()
-test(X, cutline_score(train_score_1per))
+X, train_score_1per, columns = train()
+test(X, cutline_score(train_score_1per), columns)
 
 # to_CSV(X)
