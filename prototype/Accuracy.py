8aabe1213829913daff431c2786c94cba0af0963
XENIA <lommy101@gmail.com>
2020-05-10 02:43:56 +0900

    Add files via upload

---
+++ prototype/Accuracy.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+import Preprocessing
+import matplotlib.pyplot as plt
+from sklearn.neighbors import LocalOutlierFactor
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.decomposition import PCA
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+from   sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+def NormalAccuracy(X, y):
+    clf = LocalOutlierFactor(n_neighbors=2)
+    clf.fit_predict(X)
+    y_pred = clf.fit_predict(X)
+    accuracy = accuracy_score(y, y_pred)
+    print(accuracy)
+
+path = "./CIC-output/1.csv"
+X = Preprocessing.load_df(path)
+y = np.array([0 for x in range(len(X[0:910]))] + [1 for x in range(len(X[910:1806]))] + [2 for x in range(len(X[1806:]))])
